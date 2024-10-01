8f9bb4d39209e0b50f87809bd8de0d0ecc9ab38c
arash-mohammadi-1 <arshmohammadi.komak@gmail.com>
2024-10-01 20:26:25 +0330

    be tavan two

--- assignment 3/assignment 7.py
+++ assignment 3/assignment 7.py
@@ -1,8 +1,13 @@
-vorodi = int(input("enter your numbers:"))
-vorodi = list (vorodi)
+def tavan_two(numbers) :
+    power_of_two = list(map(lambda x: x ** 2 , numbers))
+    return power_of_two
 
-def tavan_two(enters) :
-    return enters ** 2
-
-resalt = map(tavan_two,vorodi)
-print(list(resalt))
+if __name__ == "__main__":
+    while True:
+        try:
+            number_list = list(map(int, input("enter your numbers separated by space: ").split()))
+            break
+        except ValueError:
+            print("Try again you dumb blitch")
+            continue
+    print(tavan_two(number_list))
