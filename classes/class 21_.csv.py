5aee2a99c32825228026db8e3eaf986d4251bd43
arash-mohammadi-1 <arshmohammadi.komak@gmail.com>
2024-11-06 01:19:34 +0330

    classes

--- classes/class 21_.csv.py
+++ classes/class 21_.csv.py
@@ -1,6 +1,7 @@
 import csv
 
-with open("samples_for_assignment\employees.csv","r", encoding= "utf=8") as csv_file :
+csv_input_path = "samples_for_assignment\employees.csv"
+with open(csv_input_path,"r", encoding= "utf=8") as csv_file :
     reader = csv.reader(csv_file)
 
     headers = next(reader)
@@ -8,3 +9,26 @@ with open("samples_for_assignment\employees.csv","r", encoding= "utf=8") as csv_
 
     for row in reader:
         print(row)
+
+
+
+csv_output_path = "samples_for_assignment\out.csv"
+
+fields = ["Name", "Family", "Age", "Score"]
+rows = [
+    ["Arash", "mohammadi", "15", "0"],
+    ["Taha", "hokmabadi", "15", "0"],
+    ["Alireza", "Yazdani", "16", "0"],
+    ["setayesh", "shojae", "14", "0"], 
+    ["sogand", "jangi", "14", "0"],
+    ["Niayesh", "Adaszabe", "13", "0"],
+    ["mohammadhossin", "yaghoubiezade", "13", "0"],
+    ["Amieali", "Niazmand", "14", "0"],
+    ["Alireza", "mohammadi", "15", "0"],
+]
+
+with open(csv_output_path, "w") as file:
+    csv_writer = csv.writer(file)
+    csv_writer.writerow(fields)
+    for row in rows:
+        csv_writer.writerow(row)
